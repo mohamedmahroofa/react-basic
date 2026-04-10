a2883919aaca070d4af4b4d547b7d470486423b9
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-04-10 01:48:25 -0400

    Created the react prject and added dynamic instead of static.

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+const reactDescription = ["Fundamental", "Core", "Crucial"];
+
+function genRandomNum(max){
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header(){
+  const description = reactDescription[genRandomNum(2)];
+
+  return (
+    <header>
+        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <h1>React Essentials</h1>
+        <p>
+          {description} React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
