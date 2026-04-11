e36886d03567c32d52a4202338a180ef1336cc7b
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-04-11 18:25:44 -0400

    Split the components into its own files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-
+import Header from "./components/Header.jsx";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescription = ["Fundamental", "Core", "Crucial"];
-
-let descriptionLength = reactDescription.length;
-
-const randomValue = Math.floor(Math.random() * descriptionLength);
-
-
-function CoreConcepts({title, description, image}){
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-}
-
-function Header() {
-  const description = reactDescription[randomValue];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 
 function App() {
   return (
@@ -40,13 +10,13 @@ function App() {
         <section id="core-concepts">
           <ul>
             <CoreConcepts
-              image = {CORE_CONCEPTS[0].image}
-              title= {CORE_CONCEPTS[0].title}
-              description = {CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
             />
-            <CoreConcepts {...CORE_CONCEPTS[1]}/>
-            <CoreConcepts {...CORE_CONCEPTS[2]}/>
-            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import reactImg from "../assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Core", "Crucial"];
+
+let descriptionLength = reactDescription.length;
+
+const randomValue = Math.floor(Math.random() * descriptionLength);
+
+export default function Header() {
+  const description = reactDescription[randomValue];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
