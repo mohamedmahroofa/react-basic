472413a0728dc747c0947f85838e2b415baa9762
mohamedmahroofa <ahm.sheedh@gmail.com>
2026-06-03 16:19:08 -0400

    Split the compnents for Examples and Tab contents, now the app.js is leaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,14 @@
-import { useState } from "react";
 import Header from "./components/Header/Header.jsx";
-import CoreConcepts from './components/CoreConcepts.jsx';
-import TabButtons from "./components/TabButtons.jsx";
-import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if (selectedTopic) {
-    tabContent = (
-      <div id="tab-content">
-        <h3>{EXAMPLES[selectedTopic].title}</h3>
-        <p>{EXAMPLES[selectedTopic].description}</p>
-        <pre>
-          <code>{EXAMPLES[selectedTopic].code}</code>
-        </pre>
-      </div>
-    );
-  }
-
   return (
     <>
       <Header />
       <main>
         <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButtons
-              isSelected={selectedTopic === "components"}
-              onSelect={() => handleSelect("components")}
-            >
-              Components
-            </TabButtons>
-            <TabButtons
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => handleSelect("jsx")}
-            >
-              JSX
-            </TabButtons>
-            <TabButtons
-              isSelected={selectedTopic === "props"}
-              onSelect={() => handleSelect("props")}
-            >
-              Props
-            </TabButtons>
-            <TabButtons
-              isSelected={selectedTopic === "state"}
-              onSelect={() => handleSelect("state")}
-            >
-              State
-            </TabButtons>
-          </menu>
-          {tabContent}
-        </section>
+        <Examples />
       </main>
     </>
   );
